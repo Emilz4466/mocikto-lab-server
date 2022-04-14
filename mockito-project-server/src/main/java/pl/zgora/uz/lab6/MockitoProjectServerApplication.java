2285972c29224d2e0d8cdeedf44285a40bf57d5c
emil.zyberski <emil.zyberski@STREAMSOFT-0177.streamsoft.pl>
2022-04-14 14:00:36 +0200

    C0 - initial commit

---
+++ mockito-project-server/src/main/java/pl/zgora/uz/lab6/MockitoProjectServerApplication.java
@@ -0,0 +1,13 @@
+package pl.zgora.uz.lab6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoProjectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoProjectServerApplication.class, args);
+	}
+
+}
---
+++ mockito-project-server/src/test/java/pl/zgora/uz/lab6/MockitoProjectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zgora.uz.lab6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoProjectServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
