c1db002a5c5b965b1d0c01c08414216d68a9fb98
emil.zyberski <emil.zyberski@STREAMSOFT-0177.streamsoft.pl>
2022-04-16 14:48:59 +0200

    C2 -  Dto

---
+++ mockito-project-server/src/main/java/pl/zgora/uz/lab6/dto/BookDto.java
@@ -0,0 +1,12 @@
+package pl.zgora.uz.lab6.dto;
+
+import pl.zgora.uz.lab6.interfaces.Dto;
+
+public class BookDto implements Dto {
+
+	public Long id;
+	public String title;
+	public String author;
+	public int numberOfPages;
+
+}
