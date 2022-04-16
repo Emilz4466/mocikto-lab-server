b944a5f842b5d13e9243a8e7a9dd00881a731ee4
emil.zyberski <emil.zyberski@STREAMSOFT-0177.streamsoft.pl>
2022-04-16 14:50:29 +0200

    C6 - Conf

--- mockito-project-server/src/main/java/pl/zgora/uz/lab6/MockitoProjectServerApplication.java
+++ mockito-project-server/src/main/java/pl/zgora/uz/lab6/MockitoProjectServerApplication.java
@@ -2,8 +2,11 @@ package pl.zgora.uz.lab6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+
+@EnableJpaRepositories("pl.zgora.uz.lab6.interfaces")
 public class MockitoProjectServerApplication {
 
 	public static void main(String[] args) {
